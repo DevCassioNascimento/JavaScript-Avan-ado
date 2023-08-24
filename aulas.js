7333dcb5eb086846a2f7e37943138ac929bd635d
DevCassioNascimento <cassionascimentovieira@outlook.com>
2023-08-23 21:36:17 -0300

    retormo

--- aulas.js
+++ aulas.js
@@ -7,3 +7,14 @@ function sayHeloo (name) {
 sayHeloo("Marcos"); 
 sayHeloo("Paulo");
 sayHeloo("João");
+
+
+/**Retorno */
+
+function sum (n1, n2) {
+    const result = n1 + n2 
+
+    return result 
+}
+
+console.log(sum(3,1)); 
